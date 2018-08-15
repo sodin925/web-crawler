12c84828ec0e8c5718fd798647719b5073120f75
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-08-16 00:19:09 +0800

    030

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 let request = require('request');
 let fs = require('fs');
 let cheerio = require('cheerio');
+
+// let x = fs.readFileSync('./posts.json', 'utf8');
+// let y = JSON.parse(x);
+// let idList = [];
+// y.forEach(element => {
+//   idList.push(element.id);
+// });
+// console.log(idList);
+
+
 request({
   method: 'GET',
   //popular=true 為熱門文章  popular =false為最新文章
   url: 'https://www.dcard.tw/_api/forums/sex/posts?popular=true',
   // url:'https://www.dcard.tw/f/sex',
 
-}, function(error, response, body) {
-  if(error || !body) { return error };
-  let $ = cheerio.load(body);
+}, function (error, response, body) {
+  if(error || !body) { return '404'};
+  let y = JSON.parse(body);
+  // console.log(y);
+
   let result = [];
-  let titles = $('h3.PostEntry_title_H5o4d');
-  for(let i=0;i<titles.length;i++) {
-    result.push($(titles[i]).text());
+  for (i = 0; i < y.length; i++) {
+    result.push(`https://www.dcard.tw/f/sex/p/${y[i].id}-${y[i].title}`);//固定網址+id-title
   }
-  fs.writeFileSync('result.json', JSON.stringify(result))
+  console.log(result[5]);
+
+  // result.forEach
+  // fs.writeFileSync('result.json', JSON.stringify(result));
 });
